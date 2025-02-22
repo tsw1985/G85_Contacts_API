d7185a44a6e83a9878efb995b9bbcb6a38938c81
Gabriel Glez <gabrielgg85@gmail.com>
2025-02-22 21:40:12 +0000

    created script SQL

--- migrations/2025-02-22-213533_g85_contacts/up.sql
+++ migrations/2025-02-22-213533_g85_contacts/up.sql
@@ -1,3 +1,4 @@
+-- Your SQL goes here
 CREATE TABLE IF NOT EXISTS contacts (
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL,
@@ -10,4 +11,3 @@ CREATE TABLE IF NOT EXISTS users (
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
-
